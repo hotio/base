1b14e891ea136f97833d9a11aa5e3208cd768f6e
hotio <hotio@protonmail.com>
2019-12-22 19:47:49 +0100

    [CI SKIP] update

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 if [[ ${1} == "checkpackages" ]]; then
     docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t arm64v8/ubuntu:18.04 bash -c 'export DEBIAN_FRONTEND="noninteractive"; apt list --installed > /github/upstream_packages.arm64.txt'
-    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t arm32v7/ubuntu:18.04 bash -c 'export DEBIAN_FRONTEND="noninteractive"; apt list --installed > /github/upstream_packages.arm.txt'
-    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t   amd64/ubuntu:18.04 bash -c 'export DEBIAN_FRONTEND="noninteractive"; apt list --installed > /github/upstream_packages.amd64.txt'
+    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t arm64v8/ubuntu:18.04 bash -c 'apt list --installed > /github/upstream_packages.arm64.txt'
+    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t arm32v7/ubuntu:18.04 bash -c 'apt list --installed > /github/upstream_packages.arm.txt'
+    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t   amd64/ubuntu:18.04 bash -c 'apt list --installed > /github/upstream_packages.amd64.txt'
 fi
