77ff21ab129a537fadd8a10bce3a69bb89bdb022
hotio <hotio@protonmail.com>
2019-12-22 19:38:56 +0100

    [CI SKIP] add export

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ ${1} == "checkpackages" ]]; then
+    export DEBIAN_FRONTEND="noninteractive"
     docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
     docker run --rm -v "${GITHUB_WORKSPACE}":/github -t arm64v8/ubuntu:18.04 bash -c 'apt list --installed > /github/upstream_packages.arm64.txt'
     docker run --rm -v "${GITHUB_WORKSPACE}":/github -t arm32v7/ubuntu:18.04 bash -c 'apt list --installed > /github/upstream_packages.arm.txt'
