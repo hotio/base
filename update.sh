67875a692fd3abc44555703e4a8760f2b4cc89cc
hotio <hotio@protonmail.com>
2019-12-23 21:26:43 +0100

    [CI SKIP] fix sed

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ${1} == "checkdigests" ]]; then
-    image="amd64/ubuntu:18.04"   && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s#FROM .*$#FROM ${digest}#g" ./linux-amd64.Dockerfile
-    image="arm32v7/ubuntu:18.04" && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s#FROM .*$#FROM ${digest}#g" ./linux-arm.Dockerfile
-    image="arm64v8/ubuntu:18.04" && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s#FROM .*$#FROM ${digest}#g" ./linux-arm64.Dockerfile
+    image="amd64/ubuntu:18.04"   && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s#FROM .*\$#FROM ${digest}#g" ./linux-amd64.Dockerfile
+    image="arm32v7/ubuntu:18.04" && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s#FROM .*\$#FROM ${digest}#g" ./linux-arm.Dockerfile
+    image="arm64v8/ubuntu:18.04" && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s#FROM .*\$#FROM ${digest}#g" ./linux-arm64.Dockerfile
 fi
