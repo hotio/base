05dc2bf9df7d34d8f82748c7c77687e5266277eb
hotio <hotio@protonmail.com>
2019-12-23 21:20:30 +0100

    use digest in FROM

--- update.sh
+++ update.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 if [[ ${1} == "checkdigests" ]]; then
-    docker pull arm64v8/ubuntu:18.04
-    docker pull arm32v7/ubuntu:18.04
-    docker pull   amd64/ubuntu:18.04
-    docker inspect --format='{{index .RepoDigests 0}}' arm64v8/ubuntu:18.04 >  upstream_digests.txt
-    docker inspect --format='{{index .RepoDigests 0}}' arm32v7/ubuntu:18.04 >> upstream_digests.txt
-    docker inspect --format='{{index .RepoDigests 0}}'   amd64/ubuntu:18.04 >> upstream_digests.txt
+    image="amd64/ubuntu:18.04"   && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s/FROM .*$/FROM ${digest}/g" ./linux-amd64.Dockerfile
+    image="arm32v7/ubuntu:18.04" && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s/FROM .*$/FROM ${digest}/g" ./linux-arm.Dockerfile
+    image="arm64v8/ubuntu:18.04" && docker pull ${image} && digest=$(docker inspect --format='{{index .RepoDigests 0}}' ${image}) && sed -i "s/FROM .*$/FROM ${digest}/g" ./linux-arm64.Dockerfile
 fi
