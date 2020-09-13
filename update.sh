2388ea5f7d92362dcae7dc3fc8719c6241236185
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 13:43:54 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -9,4 +9,7 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}.*\$#FROM ${image}@${digest}#g" ./linux-arm.Dockerfile   && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "List installed packages..."
+    docker run --rm --entrypoint="" "${2}" apt list --installed
 fi
