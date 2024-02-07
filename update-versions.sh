9ceceae3f6c2ff018c461d4d2e99322dbfbabca3
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 08:06:11 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-pia_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/pia-foss/manual-connections/commits/master" | jq -r .sha)
-[[ -z ${pia_version} ]] && exit 0
-version_json=$(cat ./VERSION.json)
-jq '.pia_version = "'"${pia_version}"'"' <<< "${version_json}" > VERSION.json
+set -e
+pia_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/pia-foss/manual-connections/commits/master" | jq -re .sha)
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg pia_version "${pia_version//v/}" \
+    '.pia_version = $pia_version' <<< "${json}" | tee VERSION.json
