f90ef1a47fd1e516b30002dd0932f1bfc6ecdd4b
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-11 10:00:30 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -6,3 +6,10 @@ jq --sort-keys \
     --arg version_pia "${version_pia//v/}" \
     --arg version_s6 "${version_s6//v/}" \
     '.version_pia = $version_pia | .version_s6 = $version_s6' <<< "${json}" | tee VERSION.json
+
+if ! git diff --quiet; then
+    json=$(cat VERSION.json)
+    jq --sort-keys \
+        --arg version "$(date -u +'%Y%m%d%H%M%S')" \
+        '.version = $version' <<< "${json}" | tee VERSION.json
+fi
