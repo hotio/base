10c91dd396cdfe5e66d9b5bd7f66393f856f62a7
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-11 12:00:10 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -3,6 +3,7 @@ version_pia=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.githu
 version_s6=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | jq -re .tag_name) || exit 1
 json=$(cat VERSION.json)
 jq --sort-keys \
+    --arg version "${version_s6//v/}--${version_pia:0:7}" \
     --arg version_pia "${version_pia//v/}" \
     --arg version_s6 "${version_s6//v/}" \
-    '.version_pia = $version_pia | .version_s6 = $version_s6' <<< "${json}" | tee VERSION.json
+    '.version = $version | .version_pia = $version_pia | .version_s6 = $version_s6' <<< "${json}" | tee VERSION.json
