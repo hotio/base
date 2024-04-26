cefd68af756342daa02151e035cf2657ce0ca958
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-04-26 06:54:04 +0200

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 version_s6=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | jq -re .tag_name) || exit 1
 [[ -z ${version_s6} ]] && exit 0
-[[ ${version_s6} == nulll ]] && exit 0
+[[ ${version_s6} == null ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version_s6//v/}" \
