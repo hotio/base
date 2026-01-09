5c07a3a46587dbd86ca34cd0f2f5b3e1452bb4e6
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 08:33:19 +0100

    refactor: streamline version handling in scripts and remove unused version field

--- update-versions.sh
+++ update-versions.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-version_s6=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | jq -re .tag_name) || exit 1
-[[ -z ${version_s6} ]] && exit 0
-[[ ${version_s6} == null ]] && exit 0
+set -exuo pipefail
+
+version_s6=$(curl -fsSL "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | jq -re .tag_name)
 json=$(cat VERSION.json)
 jq --sort-keys \
-    --arg version "${version_s6//v/}" \
     --arg version_s6 "${version_s6//v/}" \
-    '.version = $version | .version_s6 = $version_s6' <<< "${json}" | tee VERSION.json
+    '.version_s6 = $version_s6' <<< "${json}" | tee VERSION.json
