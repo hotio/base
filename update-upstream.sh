c1be17fe9abfd4fadfb097f742c8debd12a90215
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 08:31:51 +0100

    refactor: streamline version handling in scripts

--- update-upstream.sh
+++ update-upstream.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -exuo pipefail
+
 json=$(cat VERSION.json)
 upstream_image=$(jq -re '.upstream_image' <<< "${json}")
 upstream_tag=$(jq -re '.upstream_tag' <<< "${json}")
-manifest=$(skopeo inspect --raw "docker://${upstream_image}:${upstream_tag}") || exit 1
+manifest=$(skopeo inspect --raw "docker://${upstream_image}:${upstream_tag}")
 upstream_digest_amd64=$(jq -re '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest' <<< "${manifest}")
 upstream_digest_arm64=$(jq -re '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest' <<< "${manifest}")
 jq --sort-keys \
