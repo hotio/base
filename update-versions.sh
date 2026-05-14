961df021e9647b82292a9ae336b247a3d2ee3289
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-05-14 11:19:40 +0200

    cleanup files

--- update-versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exuo pipefail
-
-version_s6=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | jq -re .tag_name)
-json=$(cat meta.json)
-jq --sort-keys \
-    --arg version_s6 "${version_s6//v/}" \
-    '.version_s6 = $version_s6' <<< "${json}" | tee meta.json
