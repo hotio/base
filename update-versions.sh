dbe31c7f720d5740c2c6d9eba886c21aff49902e
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-05-17 12:35:30 +0200

    tryout new update method for versions

--- update-versions.sh
+++ update-versions.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-set -exuo pipefail
+set -euo pipefail
 
-version_s6=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | jq -re .tag_name)
-json=$(cat meta.json)
-jq --sort-keys \
-    --arg version_s6 "${version_s6//v/}" \
-    '.version_s6 = $version_s6' <<< "${json}" | tee meta.json
+while read -r line; do
+    if [[ $line =~ _command ]]; then
+        key="${line%%=*}"
+        key="${key%_command}"
+        command="${line#*=}"
+        value=$(eval "${command}")
+        echo "Result: [${key}] [${command}] [${value}]"
+        json=$(cat meta.json)
+        jq --sort-keys --arg key "$key" --arg value "$value" '.[$key] = $value' <<< "${json}" > meta.json
+    fi
+done < <(jq -r 'to_entries[] | [(.key),.value] | join("=")' < meta.json)
