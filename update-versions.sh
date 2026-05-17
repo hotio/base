8afba9d8d8429509a6922734a99e3a20fa7f4a7a
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-05-17 13:28:37 +0200

    small tweak to version check [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -2,13 +2,11 @@
 set -euo pipefail
 
 while read -r line; do
-    if [[ $line =~ _command ]]; then
-        key="${line%%=*}"
-        key="${key%_command}"
-        command="${line#*=}"
-        value=$(eval "${command}")
-        echo "Result: [${key}] [${command}] [${value}]"
-        json=$(cat meta.json)
-        jq --sort-keys --arg key "$key" --arg value "$value" '.[$key] = $value' <<< "${json}" > meta.json
-    fi
-done < <(jq -r 'to_entries[] | [(.key),.value] | join("=")' < meta.json)
+    key="${line%%=*}"
+    key="${key%__command}"
+    command="${line#*=}"
+    value=$(eval "${command}")
+    json=$(cat meta.json)
+    jq --sort-keys --arg key "$key" --arg value "$value" '.[$key] = $value' <<< "${json}" > meta.json
+    echo "Result: [${key}] [${command}] [${value}]"
+done < <(jq -r 'to_entries[] | [(.key),.value] | join("=")' < meta.json | grep '__command')
